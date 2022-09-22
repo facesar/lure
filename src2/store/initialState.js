2823caab1fcd59bb213e8f053404ff326b390101
facesar <rostranespinoza@gmail.com>
2022-09-22 22:22:26 +0300

    files_project

--- src2/store/initialState.js
+++
@@ -1,5 +0,0 @@
-const initialState = {
-    value_1: false,
-}
-
-export default initialState;
--- src2/store/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import value_1 from './reducer_1';
-
-const reducers = combineReducers({
-    value_1,
-});
-
-export default reducers;
